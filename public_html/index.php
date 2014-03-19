2f620b35106298e142e6209cf17e2dd9af581471
xles <xles@mirakulix.org>
2014-03-19 01:02:06 +0100

    Decided on the entry point design.

--- public_html/index.php
+++ public_html/index.php
@@ -2,9 +2,6 @@
 namespace MyOrg\Application;
 require_once('../mp/mp.php');
 
-$config->paths = [
-	'appPath' => dirname(__FILE__).'/../application',
-	'mpPath' => '',
-];
+$config->paths['appPath'] = dirname(__FILE__).'/../application';
 
-$mp->run();
+$mp->run(__NAMESPACE__);
